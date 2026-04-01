c2f45fa31ea070cf7e33bb09283c7ba6d5e6d852
Temuulenpda <s.temuulen0221@gmail.com>
2026-04-01 21:49:17 +0800

    Update Navbar component for responsive display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             <a href="#">Contact</a>
           </div>
         </div>
-        <div className="flex w-50 mr-60 md:block sm:hidden ">
+        <div className="flex w-50 mr-60 hidden md:block ">
           <button className="bg-white w-20 h-10 rounded-xl">Sign Up</button>
           <button className="bg-[#8b3dff] w-20 h-10 rounded-xl text-white">
             Login
@@ -42,7 +42,7 @@ const Navbar = () => {
         )}
       </div>
       {activeLink && (
-        <div className="flex flex-col md:hidden bg-indigo-700 shadow-sm  p-4">
+        <div className="flex flex-col block md:hidden bg-indigo-700 shadow-sm  p-4">
           <a href="#">Home</a>
           <a href="#">Courses</a>
           <a href="#">Teacher</a>
