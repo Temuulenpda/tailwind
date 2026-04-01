edb4a2e5d5f55d7ddfebd1ac0a854480ce3149b9
Temuulenpda <s.temuulen0221@gmail.com>
2026-04-01 21:41:28 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
   const [activeLink, setActiveLink] = useState("true");
   return (
     <div>
-      <div className="bg-gray-300 shadow-sm p-2 flex justify-between items-center">
+      <div className="bg-indigo-300 shadow-sm p-2 flex justify-between items-center">
         <div className="flex items-center">
           <div className="font-extrabold text-purple-500 text-xl md:ml-20">
             EduSmart
           </div>
         </div>
-        <div className="flex sm:hidden md:block">
+        <div className="flex hidden md:block">
           <div className="flex gap-3">
             <a href="#">Home</a>
             <a href="#">Coursed</a>
@@ -42,7 +42,7 @@ const Navbar = () => {
         )}
       </div>
       {activeLink && (
-        <div className="flex flex-col md:hidden md:block bg-gray-300 shadow-sm  p-4">
+        <div className="flex flex-col md:block bg-gray-300 shadow-sm  p-4">
           <a href="#">Home</a>
           <a href="#">Courses</a>
           <a href="#">Teacher</a>
